cd019c2741021cf665ea185e16437baef3ae57ce
Juan Ojeda <96923856+NightIQDev@users.noreply.github.com>
2022-05-15 21:48:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,450 @@
+const anim = [
+    {   
+        id:'llamar',
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'karaka',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'loco',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+    {   
+        id:1,
+        name:'Aplaudir',
+        cmd: '/e clap',
+    },
+
+]
+
+let allDiv = document.querySelector('.lol')
+
+let divi = document.querySelector('.all')
+let num = 0
+allDiv.addEventListener('click', ()=>{
+    num++
+    divi.setAttribute('style', 'display:grid;')
+    if(num == 2){
+        divi.setAttribute('style', 'display:none;')
+        num = 0
+    }
+})
+
+
+
+
+
+let dadCont = document.querySelector('.div-cont')
+
+const form = document.querySelector('.text-l')
+
+const btonS = document.querySelector('.buscar')
+let div;
+const all = anim.map((e)=>{
+    div = document.createElement('div')
+    div.innerHTML += `<p>${e.name}</p><p class='yellow'>${e.cmd}</p>`;
+    dadCont.appendChild(div) 
+})
+
+form.addEventListener('keyup', ()=>{
+    
+    const text = form.value.toLowerCase()
+    anim.map(e => {
+    let nameS = e.name.toLowerCase()
+        if(text.indexOf(nameS) !== -1){    
+            dadCont.innerHTML = `<div class='rr'>${e.name}<p class='yellow'>${e.cmd}</p></div>`
+        }
+
+    })
+})
+
